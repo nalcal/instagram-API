923dcfbce7a259fdd4f71f7df38fb189de049128
Nalitha Liyanage <nalitha770@gmail.com>
2015-05-11 19:55:55 -0700

    m 6

--- index.php
+++ index.php
@@ -11,6 +11,17 @@ define('clientID', '31197fed7f9e44b9a89555d336819848');
 define('clientSecret', '13131ef5975543f89af94cde7a805535');
 define('redirectURI', 'http://localhost/apinalitha/index.php');
 define('ImageDirectory', 'pics/');
+
+if isset(($_GET['code'])){
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_settings = array('client_id' => clientID,
+									'client_secret' => clientSecret,
+									'grant_type' => 'authorization_code',
+									'redirect_uri' => redirectURI,
+									'code' => $code
+									);
+}
 ?>
 
 <!DOCTYPE html>
